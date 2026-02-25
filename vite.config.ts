1d6cede0a182314a5c61b831599a2c0059acde29
Jenamari Bathan <jenamaribathan@gmail.com>
2026-02-25 23:00:09 +1100

    Set package type to module to fix ESM build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/app/components"),
-      "@pages": path.resolve(__dirname, "./src/app/pages"),
+      "@": path.resolve(new URL('.', import.meta.url).pathname, "./src"),
     },
   },
-})
+});
